6b449c54a9db5091b6fb131657b23cdc64dd2370
thierry-a <thierryarzal353@gmail.com>
2020-04-20 09:42:38 +0200

    Adding quote, image, name on display and a button fetching a the api for a random quote

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import QuoteCard from './components/QuoteCard';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const sampleQuote = {
+  quote: 'Gah, stupid sexy Flanders!',
+  character: 'Homer simpson',
+  image: 'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939',
+ };
+
+
+  class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        quote: sampleQuote
+      };
+      this.getQuote = this.getQuote.bind(this); 
+    }
+  
+    getQuote() {
+      // Send the request
+      axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+        // Extract the DATA from the received response
+        .then(response => response.data)
+        // Use this data to update the state
+        .then(data => {
+          this.setState({
+            quote: data[0],
+          });
+      });
+    }  
+  
+    render() {
+      return (
+        <div className="App">
+          <QuoteCard quote={this.state.quote} />
+          <button type="button" onClick={this.getQuote}>Get quote</button>
+        </div>
+      );
+    }
+  }
+  
+
+
 
 export default App;
